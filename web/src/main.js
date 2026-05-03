7b15cff121371c4b97d3a007817f44b421f4718e
xiaoxi <xswork@qq.com>
2026-05-03 18:46:09 +0800

    refactor(web): 抽取 Vuetify 配置到独立插件文件

--- web/src/main.js
+++ web/src/main.js
@@ -2,20 +2,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
-
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css'
-
+import vuetify from './plugins/vuetify'
 import App from './App.vue'
-
-const vuetify = createVuetify({
-  components,
-  directives,
-})
 
 const app = createApp(App)
 app.use(createPinia())
